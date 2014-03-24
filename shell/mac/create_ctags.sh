4a8c3ab0c4bdcbb031ae7dc95dd7ad4781825b65
bundai223 <bundai223@gmail.com>
2014-03-24 13:11:43 +0900

    add basic create tag script.

--- shell/mac/create_ctags.sh
+++ shell/mac/create_ctags.sh
@@ -1,45 +1,6 @@
-# !sh
-#=====================================================
-# project用のtagファイルを生成するスクリプト
-#=====================================================
+# !/bin/bash
 
-#DIRNAME=`dirname $0`
-#PATH_TO_HERE=`readlink $DIRNAME`
-#PROJECT_ROOT=$PATH_TO_HERE/..
-PROJECT_ROOT=$PWD
+TAGFILE_DIR=`dirName $0`
 
-
-# タグファイル場所
-TAG_CREATE_DIR=${PROJECT_ROOT}/graphic_base
-TAG_FILE="$TAG_CREATE_DIR/tags"
-
-# ライブラリパス
-TAGS_EXTEND_DIR_LIST=" ${PROJECT_ROOT}/lib/ml"
-TAGS_EXTEND_DIR_LIST+=" /usr/local/include/"
-
-
-# set env
-CTAGS=ctags
-
-
-TAGS_LANGMAP='--langmap=c++:+.tbl:.inl'
-
-
-# create tag file
-echo "create ctags ..."
-$CTAGS -f $TAG_FILE --excmd=n $TAGS_LANGMAP -R $TAG_CREATE_DIR
-
-# check lang map
-# echo "check langmap ..."
-# $CTAGS -f $TAG_FILE --excmd=n -R $TAG_CREATE_DIR --list-maps=c++
-
-
-# append extend directory
-echo "append extend directory ..."
-for ex_dir in $TAGS_EXTEND_DIR_LIST; do
-    if [ -d $ex_dir ]; then
-        echo " add $ex_dir"
-        $CTAGS --append=yes -f $TAG_FILE --excmd=n $TAGS_LANGMAP -R $ex_dir
-    fi
-done
+ctags -f ${TAGFILE_DIR}/tags -R ${TAGFILE_DIR}
 
